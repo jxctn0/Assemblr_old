c98c0394cde7755cd9c71c49cb6c8f1d1e1251ee
joshua-cotugno <jojotraingeek@gmail.com>
2023-07-03 13:32:53 +0100

    Finished basic version of printer.py

--- Tests/02 - Printing out Glyphs/printer.py
+++ Tests/02 - Printing out Glyphs/printer.py
@@ -1,11 +1,24 @@
-#!/bin/env/ Python 3
+#!/bin/env python3
 
 import argparse
 
 argparser = argparse.ArgumentParser(description="Prints out given sprites")
 argparser.add_argument("file", help="File to print out")
+argparser.add_argument("-c", "--color", help="Color to print out glyphs in (0-255)")
 args = argparser.parse_args()
 
-with open(args.file) as file:
-    for line in file:
-        print(line, end="")
+VERSION = "0.0.1" 
+
+with open(args.file) as f:
+    if args.color is not None:
+        # print out color escape
+        print("\033[38;5;{}m".format(args.color))
+    # print out all glyphs in file, handling any inline variables in {}
+    for line in f:
+        line = line.strip()
+        if line.startswith("#") or line == "":
+            continue
+        print(line.format(**globals()))
+    # print out color reset escape
+    if args.color is not None:
+        print("\033[0m")
