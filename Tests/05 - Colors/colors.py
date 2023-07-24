934d16a2bc87984400fe088998929b7152294d87
Joshua Cotugno <jojotraingeek@gmail.com>
2023-07-24 13:25:07 +0100

    Changed class names for brevity

--- Tests/05 - Colors/colors.py
+++ Tests/05 - Colors/colors.py
@@ -1,6 +1,6 @@
 # Create colors class to color text
 class colors:
-    class foreground:
+    class fg:
         # Colors for text
         black = "\u001b[30m"
         red = "\u001b[31m"
@@ -11,8 +11,8 @@ class colors:
         cyan = "\u001b[36m"
         white = "\u001b[37m"
 
-    class background:
-        # Colors for background
+    class bg:
+        # Colors for bg
         black = "\u001b[40m"
         red = "\u001b[41m"
         green = "\u001b[42m"
@@ -22,7 +22,7 @@ class colors:
         cyan = "\u001b[46m"
         white = "\u001b[47m"
 
-    class formatting:
+    class fmt:
         """
         0	Reset all attributes
         1	Bold (increased intensity)
@@ -31,7 +31,7 @@ class colors:
         4	Underline
         5	Slow Blink
         6	Rapid Blink
-        7	Swap foreground and background colors (reverse format)
+        7	Swap fg and bg colors (reverse format)
         8	Hide (do not display text)
         9	Strikethrough
         """
@@ -46,72 +46,76 @@ class colors:
         hide = "\u001b[8m"
         strikethrough = "\u001b[9m"
 
-# Tests for all colors and formatting
+# Tests for all colors and fmt
 
-# foreground colors
-print(colors.foreground.black + "BLACK" + colors.formatting.reset)
-print(colors.foreground.red + "RED" + colors.formatting.reset)
-print(colors.foreground.green + "GREEN" + colors.formatting.reset)
-print(colors.foreground.yellow + "YELLOW" + colors.formatting.reset)
-print(colors.foreground.blue + "BLUE" + colors.formatting.reset)
-print(colors.foreground.purple + "PURPLE" + colors.formatting.reset)
-print(colors.foreground.cyan + "CYAN" + colors.formatting.reset)
-print(colors.foreground.white + "WHITE" + colors.formatting.reset)
+# fg colors
+print(colors.fg.black + "BLACK" + colors.fmt.reset)
+print(colors.fg.red + "RED" + colors.fmt.reset)
+print(colors.fg.green + "GREEN" + colors.fmt.reset)
+print(colors.fg.yellow + "YELLOW" + colors.fmt.reset)
+print(colors.fg.blue + "BLUE" + colors.fmt.reset)
+print(colors.fg.purple + "PURPLE" + colors.fmt.reset)
+print(colors.fg.cyan + "CYAN" + colors.fmt.reset)
+print(colors.fg.white + "WHITE" + colors.fmt.reset)
 
-# background colors
-print(colors.background.black + "BLACK" + colors.formatting.reset)
-print(colors.background.red + "RED" + colors.formatting.reset)
-print(colors.background.green + "GREEN" + colors.formatting.reset)
-print(colors.background.yellow + "YELLOW" + colors.formatting.reset)
-print(colors.background.blue + "BLUE" + colors.formatting.reset)
-print(colors.background.purple + "PURPLE" + colors.formatting.reset)
-print(colors.background.cyan + "CYAN" + colors.formatting.reset)
-print(colors.background.white + "WHITE" + colors.formatting.reset)
+# bg colors
+print(colors.bg.black + "BLACK" + colors.fmt.reset)
+print(colors.bg.red + "RED" + colors.fmt.reset)
+print(colors.bg.green + "GREEN" + colors.fmt.reset)
+print(colors.bg.yellow + "YELLOW" + colors.fmt.reset)
+print(colors.bg.blue + "BLUE" + colors.fmt.reset)
+print(colors.bg.purple + "PURPLE" + colors.fmt.reset)
+print(colors.bg.cyan + "CYAN" + colors.fmt.reset)
+print(colors.bg.white + "WHITE" + colors.fmt.reset)
 
-# formatting
-print(colors.formatting.bold + "BOLD" + colors.formatting.reset)
-print(colors.formatting.dim + "DIM" + colors.formatting.reset)
-print(colors.formatting.italic + "ITALIC" + colors.formatting.reset)
-print(colors.formatting.underline + "UNDERLINE" + colors.formatting.reset)
-print(colors.formatting.slow_blink + "SLOW_BLINK" + colors.formatting.reset)
-print(colors.formatting.rapid_blink + "RAPID_BLINK" + colors.formatting.reset)
-print(colors.formatting.reverse + "REVERSE" + colors.formatting.reset)
-print(colors.formatting.hide + "HIDE" + colors.formatting.reset)
-print(colors.formatting.strikethrough + "STRIKETHROUGH" + colors.formatting.reset)
+# fmt
+print(colors.fmt.bold + "BOLD" + colors.fmt.reset)
+print(colors.fmt.dim + "DIM" + colors.fmt.reset)
+print(colors.fmt.italic + "ITALIC" + colors.fmt.reset)
+print(colors.fmt.underline + "UNDERLINE" + colors.fmt.reset)
+print(colors.fmt.slow_blink + "SLOW_BLINK" + colors.fmt.reset)
+print(colors.fmt.rapid_blink + "RAPID_BLINK" + colors.fmt.reset)
+print(colors.fmt.reverse + "REVERSE" + colors.fmt.reset)
+print(colors.fmt.hide + "HIDE" + colors.fmt.reset)
+print(colors.fmt.strikethrough + "STRIKETHROUGH" + colors.fmt.reset)
 
-# tests for combining foreground colors and background colors
-print(colors.foreground.red + colors.background.blue + "RED + BLUE_BG" + colors.formatting.reset)
-print(colors.foreground.green + colors.background.yellow + "GREEN + YELLOW_BG" + colors.formatting.reset)
-print(colors.foreground.yellow + colors.background.green + "YELLOW + GREEN_BG" + colors.formatting.reset)
-print(colors.foreground.blue + colors.background.red + "BLUE + RED_BG" + colors.formatting.reset)
+# tests for combining fg colors and bg colors
+print(colors.fg.red + colors.bg.blue + "RED + BLUE_BG" + colors.fmt.reset)
+print(colors.fg.green + colors.bg.yellow + "GREEN + YELLOW_BG" + colors.fmt.reset)
+print(colors.fg.yellow + colors.bg.green + "YELLOW + GREEN_BG" + colors.fmt.reset)
+print(colors.fg.blue + colors.bg.red + "BLUE + RED_BG" + colors.fmt.reset)
 
-# tests for combining foreground colors and formatting
-print(colors.foreground.red + colors.formatting.bold + "RED + BOLD" + colors.formatting.reset)
-print(colors.foreground.green + colors.formatting.italic + "GREEN + ITALIC" + colors.formatting.reset)
-print(colors.foreground.yellow + colors.formatting.underline + "YELLOW + UNDERLINE" + colors.formatting.reset)
-print(colors.foreground.blue + colors.formatting.slow_blink + "BLUE + SLOW_BLINK" + colors.formatting.reset)
-print(colors.foreground.purple + colors.formatting.rapid_blink + "PURPLE + RAPID_BLINK" + colors.formatting.reset)
-print(colors.foreground.cyan + colors.formatting.reverse + "CYAN + REVERSE" + colors.formatting.reset)
-print(colors.foreground.white + colors.formatting.hide + "WHITE + HIDE" + colors.formatting.reset)
-print(colors.foreground.black + colors.formatting.strikethrough + "BLACK + STRIKETHROUGH" + colors.formatting.reset)
+# tests for combining fg colors and fmt
+print(colors.fg.red + colors.fmt.bold + "RED + BOLD" + colors.fmt.reset)
+print(colors.fg.green + colors.fmt.italic + "GREEN + ITALIC" + colors.fmt.reset)
+print(colors.fg.yellow + colors.fmt.underline + "YELLOW + UNDERLINE" + colors.fmt.reset)
+print(colors.fg.blue + colors.fmt.slow_blink + "BLUE + SLOW_BLINK" + colors.fmt.reset)
+print(colors.fg.purple + colors.fmt.rapid_blink + "PURPLE + RAPID_BLINK" + colors.fmt.reset)
+print(colors.fg.cyan + colors.fmt.reverse + "CYAN + REVERSE" + colors.fmt.reset)
+print(colors.fg.white + colors.fmt.hide + "WHITE + HIDE" + colors.fmt.reset)
+print(colors.fg.black + colors.fmt.strikethrough + "BLACK + STRIKETHROUGH" + colors.fmt.reset)
 
-# tests for combining background colors and formatting
-print(colors.background.red + colors.formatting.bold + "RED_BG + BOLD" + colors.formatting.reset)
-print(colors.background.green + colors.formatting.italic + "GREEN_BG + ITALIC" + colors.formatting.reset)
-print(colors.background.yellow + colors.formatting.underline + "YELLOW_BG + UNDERLINE" + colors.formatting.reset)
-print(colors.background.blue + colors.formatting.slow_blink + "BLUE_BG + SLOW_BLINK" + colors.formatting.reset)
-print(colors.background.purple + colors.formatting.rapid_blink + "PURPLE_BG + RAPID_BLINK" + colors.formatting.reset)
-print(colors.background.cyan + colors.formatting.reverse + "CYAN_BG + REVERSE" + colors.formatting.reset)
-print(colors.background.white + colors.formatting.hide + "WHITE_BG + HIDE" + colors.formatting.reset)
-print(colors.background.black + colors.formatting.strikethrough + "BLACK_BG + STRIKETHROUGH" + colors.formatting.reset)
+# tests for combining bg colors and fmt
+print(colors.bg.red + colors.fmt.bold + "RED_BG + BOLD" + colors.fmt.reset)
+print(colors.bg.green + colors.fmt.italic + "GREEN_BG + ITALIC" + colors.fmt.reset)
+print(colors.bg.yellow + colors.fmt.underline + "YELLOW_BG + UNDERLINE" + colors.fmt.reset)
+print(colors.bg.blue + colors.fmt.slow_blink + "BLUE_BG + SLOW_BLINK" + colors.fmt.reset)
+print(colors.bg.purple + colors.fmt.rapid_blink + "PURPLE_BG + RAPID_BLINK" + colors.fmt.reset)
+print(colors.bg.cyan + colors.fmt.reverse + "CYAN_BG + REVERSE" + colors.fmt.reset)
+print(colors.bg.white + colors.fmt.hide + "WHITE_BG + HIDE" + colors.fmt.reset)
+print(colors.bg.black + colors.fmt.strikethrough + "BLACK_BG + STRIKETHROUGH" + colors.fmt.reset)
 
-# tests for combining foreground colors, background colors, and formatting
-print(colors.foreground.red + colors.background.blue + colors.formatting.bold + "RED + BLUE_BG + BOLD" + colors.formatting.reset)
-print(colors.foreground.green + colors.background.yellow + colors.formatting.italic + "GREEN + YELLOW_BG + ITALIC" + colors.formatting.reset)
-print(colors.foreground.yellow + colors.background.green + colors.formatting.underline + "YELLOW + GREEN_BG + UNDERLINE" + colors.formatting.reset)
-print(colors.foreground.blue + colors.background.red + colors.formatting.slow_blink + "BLUE + RED_BG + SLOW_BLINK" + colors.formatting.reset)
-print(colors.foreground.purple + colors.background.cyan + colors.formatting.rapid_blink + "PURPLE + CYAN_BG + RAPID_BLINK" + colors.formatting.reset)
-print(colors.foreground.cyan + colors.background.purple + colors.formatting.reverse + "CYAN + PURPLE_BG + REVERSE" + colors.formatting.reset)
-print(colors.foreground.white + colors.background.black + colors.formatting.hide + "WHITE + BLACK_BG + HIDE" + colors.formatting.reset)
-print(colors.foreground.black + colors.background.white + colors.formatting.strikethrough + "BLACK + WHITE_BG + STRIKETHROUGH" + colors.formatting.reset)
+# tests for combining fg colors, bg colors, and fmt
+print(colors.fg.red + colors.bg.blue + colors.fmt.bold + "RED + BLUE_BG + BOLD" + colors.fmt.reset)
+print(colors.fg.green + colors.bg.yellow + colors.fmt.italic + "GREEN + YELLOW_BG + ITALIC" + colors.fmt.reset)
+print(colors.fg.yellow + colors.bg.green + colors.fmt.underline + "YELLOW + GREEN_BG + UNDERLINE" + colors.fmt.reset)
+print(colors.fg.blue + colors.bg.red + colors.fmt.slow_blink + "BLUE + RED_BG + SLOW_BLINK" + colors.fmt.reset)
+print(colors.fg.purple + colors.bg.cyan + colors.fmt.rapid_blink + "PURPLE + CYAN_BG + RAPID_BLINK" + colors.fmt.reset)
+print(colors.fg.cyan + colors.bg.purple + colors.fmt.reverse + "CYAN + PURPLE_BG + REVERSE" + colors.fmt.reset)
+print(colors.fg.white + colors.bg.black + colors.fmt.hide + "WHITE + BLACK_BG + HIDE" + colors.fmt.reset)
+print(colors.fg.black + colors.bg.white + colors.fmt.strikethrough + "BLACK + WHITE_BG + STRIKETHROUGH" + colors.fmt.reset)
 
+
+# Make "presets" for colors & fmt
+class presets:
+    screen = colors.fg.white + colors.bg.black + colors.fmt.reset
